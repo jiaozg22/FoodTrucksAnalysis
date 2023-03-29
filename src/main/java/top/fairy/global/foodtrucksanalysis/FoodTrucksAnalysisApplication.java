4fcda7137991b4ce3d25eee400470fb1062f54b5
jiaozg22 <jiao_zg22@163.com>
2023-03-29 21:58:22 +0800

    项目初始化

---
+++ src/main/java/top/fairy/global/foodtrucksanalysis/FoodTrucksAnalysisApplication.java
@@ -0,0 +1,13 @@
+package top.fairy.global.foodtrucksanalysis;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FoodTrucksAnalysisApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FoodTrucksAnalysisApplication.class, args);
+    }
+
+}
---
+++ src/test/java/top/fairy/global/foodtrucksanalysis/FoodTrucksAnalysisApplicationTests.java
@@ -0,0 +1,13 @@
+package top.fairy.global.foodtrucksanalysis;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FoodTrucksAnalysisApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
